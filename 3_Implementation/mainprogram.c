cfa516eec00eacb58efaf79d3a2a7a63ef10d2eb
Teja-Srinivas-Reddy <102644653+Teja-Srinivas-Reddy@users.noreply.github.com>
2022-03-30 12:37:48 +0530

    Add files via upload

---
+++ 3_Implementation/mainprogram.c
@@ -0,0 +1,31 @@
+#include"methodoperations.h"
+
+void printTitle();
+void getDetails();
+int getMenuOption();
+void showAllEntries();
+void addNewEntry();
+int main()
+{
+
+  int menu;
+  while (1) 
+  {
+    menu = getMenuOption();
+    if (menu == 1) 
+    {
+      // Add new entry
+      addNewEntry();
+    }
+    else if (menu == 2) 
+    {
+      // Show All entries and exit
+      showAllEntries();
+    } 
+    else 
+    {
+      // exit from app.
+      break;
+    }
+  }
+}
