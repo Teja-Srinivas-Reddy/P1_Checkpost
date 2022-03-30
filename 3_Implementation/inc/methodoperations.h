67de94c01b39cbf8d2836cfa795ee58faf62f0c5
Teja-Srinivas-Reddy <102644653+Teja-Srinivas-Reddy@users.noreply.github.com>
2022-03-30 12:37:07 +0530

    Add files via upload

---
+++ 3_Implementation/inc/methodoperations.h
@@ -0,0 +1,15 @@
+#ifdef  __METHODOPERATIONS_H
+#define __METHODOPERATIONS_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+void printTitle();
+void getDetails();
+int getMenuOption();
+void showAllEntries();
+
+void addNewEntry();
+
+#endif
